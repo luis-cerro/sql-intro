418c1da9ac77854804d4f61570ef8edb3e5081b0
Luis Cerro <luis.cerro@kellogg.northwestern.edu>
2023-01-17 20:05:50 +0000

    sql_intro_2

--- school-4-alter-drop-column.sql
+++ school-4-alter-drop-column.sql
@@ -1,2 +1,2 @@
 -- Drop the phone number column
-
+ALTER TABLE sutudents DROP COLUMN phone;
---
+++ school-5-insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO students (
+    "first_name",
+    "last_name",
+    "email"
+) VALUES (
+    "Luis",
+    "Cerro",
+    "luis.cerro@kellogg.northwestern.edu"
+)
